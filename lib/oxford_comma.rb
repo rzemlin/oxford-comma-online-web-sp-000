ea7e1da7e864df2f81a62efb6b60bc373ddcfe4d
Ryan Zemlin <rzemlin@gmail.com>
2021-02-13 16:32:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
     when array 1
       "#{array[0]}"
     when 2
-      "array[0,1].join (" and ")"
+      array[0,1].join (" and ")
     else
-      "#{array[0...-1]}"".join ("' ") << ", and #{array[-1]}"
+      array[0...-1].join ("' ") << ", and #{array[-1]}"
   end
 end
