3bbc401b28a9fdc4e1c6b37acce46c5b45d77e66
Ryan Zemlin <rzemlin@gmail.com>
2021-02-13 16:51:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,10 @@
 def oxford_comma(array)
-    if array.size >= 3 
+  case array.size
+    when 0
+      ""
+    when 1
+      
+  if array.size >= 3 
         array[0..-1].join ("' ") << ", and #{array[-1]}" 
       elsif array.size == 2
         array[0,1].join (" and ")
