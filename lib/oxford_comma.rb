dadec529629f3acd078496198c93fc2e84a20cbc
Ryan Zemlin <rzemlin@gmail.com>
2021-02-13 16:40:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   case array.length
-    when array 1
+    when array (1)
       "#{array[0]}"
-    when array 2
+    when array (2)
       array[0,1].join (" and ")
     else
       array[0...-1].join ("' ") << ", and #{array[-1]}"
