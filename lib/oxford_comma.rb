2930c9d315237aeee2bb23cad7dad8614d09e362
Ryan Zemlin <rzemlin@gmail.com>
2021-02-13 15:34:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ def oxford_comma(array)
     when array 1
       array[0]
     when 2
-      array.join ("and")
+      array[0,1].join ("and")
     else
+      array[0...-1].join ("' ") << array[-1].join ("and ")
 end
