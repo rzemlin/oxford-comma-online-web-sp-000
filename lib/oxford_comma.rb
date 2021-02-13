b0d53df300f60f1c2ecb588902c616d54135bc97
Ryan Zemlin <rzemlin@gmail.com>
2021-02-13 16:45:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-    if array.length == 1
-      "#{array[0]}"
-    elsif array.length == 2
-      array[0,1].join (" and ")
+    if array.length > 2 
+      array[0..-1].join ("' ") << ", and #{array[-1]}" 
+        elsif array.length == 2
+        array[0,1].join (" and ")
     else
-      array[0...-1].join ("' ") << ", and #{array[-1]}"
+        "#{array[0]}"
   end
 end
