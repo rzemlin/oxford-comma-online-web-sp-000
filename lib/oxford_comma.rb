700836832cf96383528b6e80314f3915f3189931
Ryan Zemlin <rzemlin@gmail.com>
2021-02-13 16:55:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,12 +7,6 @@ def oxford_comma(array)
     when 2
       array.join (" and ")
     else
-      
-  if array.size >= 3 
-        array[0..-1].join ("' ") << ", and #{array[-1]}" 
-      elsif array.size == 2
-        array[0,1].join (" and ")
-      else
-        "#{array[0]}"
+      array[0...-1].join("' ") << array.last (" and ")
   end
 end
